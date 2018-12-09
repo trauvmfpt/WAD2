e7e595c726ca236a9a3a85751d7720de5520f6de
Tra <trauvmd00621@fpt.edu.vn>
2018-12-09 21:41:18 +0700

    Initial Commit

---
+++ WebApplication3/Data/WebApplication3Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Models
+{
+    public class WebApplication3Context : DbContext
+    {
+        public WebApplication3Context (DbContextOptions<WebApplication3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication3.Models.User> User { get; set; }
+    }
+}
---
+++ WebApplication3/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.CreatedAt = DateTime.Now;
+            this.UpdatedAt = DateTime.Now;
+            this.Status = UserStatus.Active;
+        }
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public UserStatus Status { get; set; }
+    }
+
+    public enum UserStatus
+    {
+        Active = 1,
+        Deactive = 0
+    }
+}
